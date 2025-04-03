5361d53df5a5eaa093014faf2f0a635499f55833
Mlazarovab <x24147087@student.ncirl.ie>
2025-04-03 17:23:02 +0100

    integrating OWASP ZAP scan for frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 // declare & initialize var for the api
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = '/api'; //changed 
 
 const api = {
   //async function getAllContacts to retrieve the contacts from the backend
   getAllContacts: async () => {
     try {
-      const response = await axios.get(`${API_BASE_URL}/contacts`); //http get request to the api
+      const response = await axios.get(`${API_BASE_URL}/contacts`, { //http get request to the api
+          withCredentials: true // send cookies
+      });
       return response.data; // if request is successful, the method will return the data from the response
     } catch (error) { // catching errors
       console.error("Error fetching contacts:", error); // logs the error message to the console
@@ -18,7 +20,9 @@ const api = {
   // asyn function addContact accepting contactData as a parameter
   addContact: async (contactData) => {
     try {
-      const response = await axios.post(`${API_BASE_URL}/contacts`, contactData); //http post request passing the contactData parameter
+      const response = await axios.post(`${API_BASE_URL}/contacts`, contactData, { ////http post request passing the contactData parameter
+        withCredentials: true
+      });
       return response.data;
     } catch (error) {
       console.error("Error adding contact:", error);
